e3c5f7f43ea8ccf1d298af9b13f106534ab7bb62
DJOHNSON02 <dylson1702@gmail.com>
2021-03-09 10:23:17 -0400

    March 9th, 2021

---
+++ StaticClassDemo/StaticClassDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using StaticClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = CustomPrinter.QuoteIt("640K ought to be enough memory for anyone", "Bill Gates");
+            Console.WriteLine(quote);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticClassDemo/StaticClassLibrary/CustomPrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassLibrary
+{
+    public static class CustomPrinter
+    {
+        public static string QuoteIt(string theQuote, string author) //Anything in a static class must also use static keyword
+        {
+            return $"'{theQuote}' - {author}";
+        }
+    }
+}
